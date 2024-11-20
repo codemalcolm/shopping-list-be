529fa906f3ad41a4de26c855421efe841d50bfd4
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-11-20 21:31:47 +0100

    createShoppingList updated with proper response

--- controllers/shoppingLists.js
+++ controllers/shoppingLists.js
@@ -4,19 +4,37 @@ const asyncWrapper = require("../middleware/async")
 // SHOPPING LISTS
 
 const createShoppingList = asyncWrapper(async (req, res) => {
-    const shoppingList = req.body
-    console.log(shoppingList)
-    res.status(201).json(shoppingList)
+    const { name, owner, memberList, itemList } = req.body;
+
+    const processedItems = itemList.map((item) => ({
+        ...item,
+        isDone: item.isDone || false, // Setting isDone to false if not provided
+      }));
+
+    const shoppingList = {
+        name,
+        state: "active", // Default value
+        owner,
+        memberList,
+        itemList: processedItems,
+        isDone: false, // Default value
+        isArchived: false // Default value
+      };
+
+    res.status(201).json({
+        message: "Shopping list created successfully",
+        data: shoppingList
+      });
 })
 
 const getAllShoppingLists = asyncWrapper(async (req,res) => {
     const shoppingLists = await req.body
-    res.status(201).json(shoppingLists)
+    res.status(201).json("getAllShoppingLists")
 })
 
 const editShoppingList = asyncWrapper(async (req,res)=>{
     const { id: taskID } = req.params;
-    res.status(201).json(req.body)
+    res.status(201).json("editShoppingList")
 })
 
 const deleteShoppingList = asyncWrapper(async (req,res)=>{
