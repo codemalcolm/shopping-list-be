aef031721c815d2dfc1bfabaf6ae71ee829337fc
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-11-23 18:45:18 +0100

    Added item validation function

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,14 +1,25 @@
-const {shoppingListValidationSchema} = require("../validators/validation")
+const {shoppingListValidationSchema, shoppingListItemValidationSchema} = require("../validators/validationSchemas")
 
 const validateShoppingList = (req, res, next) => {
-    const { error } = shoppingListValidationSchema.validate(req.body, { abortEarly: false });
-    if (error) {
-      return res.status(400).json({
-        error: "Validation error",
-        details: error.details.map((err) => err.message)
-      });
-    }
-    next();
-  };
+  const { error } = shoppingListValidationSchema.validate(req.body, { abortEarly: false });
+  if (error) {
+    return res.status(400).json({
+      error: "Validation error",
+      details: error.details.map((err) => err.message)
+    });
+  }
+  next();
+};
 
-module.exports = {validateShoppingList}
+const validateShoppingListItem = (req, res, next) => {
+  const { error } = shoppingListItemValidationSchema.validate(req.body, { abortEarly: false });
+  if (error) {
+    return res.status(400).json({
+      error: "Validation error",
+      details: error.details.map((err) => err.message)
+    });
+  }
+  next();
+};
+
+module.exports = {validateShoppingList,validateShoppingListItem}
