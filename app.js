25685a7a469e67936c9fa578b9ac6db76111f060
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-11-25 13:13:53 +0100

    Connected with db

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const app = express();
-const shoppingLists = require("./routes/shoppingLists")
-
-const port = 3000;
-
+const shoppingLists = require("./routes/shoppingLists");
+const connectDB = require("./db/connect");
+require("dotenv").config()
 
 app.use(express.json()); // Parse JSON bodies
 
-app.get("/", (req, res)=> {
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 // routes
-app.use("/shoppinglist", shoppingLists)
+app.use("/shoppinglist", shoppingLists);
 
+const port = 3000;
 
-app.listen(port, () => {
-	console.log(`Server is started on http://localhost:${port}`);
-});
+const start = async () => {
+	try {
+		// mongo connection
+		await connectDB(process.env.MONGO_URI); // Connecting to DB
+
+		// port listening
+		app.listen(port, () => {
+			console.log(`Server is started on http://localhost:${port}`);
+		});
+	} catch (error) {}
+};
+
+start();
